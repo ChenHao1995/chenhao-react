55ce53cfdad249ef2d366d47d9d4ec0cf6aa66e7
陈浩 <chenhao@chenhaodeMacBook-Pro.local>
2019-01-04 17:20:05 +0800

    修改服务

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-var express = require('express')
-var app = express()
-var proxy = require('http-proxy-middleware')
-var consolidate = require('consolidate')
-var open = require('open')
+var express = require("express");
+var app = express();
+var proxy = require("http-proxy-middleware");
+var consolidate = require("consolidate");
+var open = require("open");
 
-var port = 6868
+var port = 6868;
 
-app.engine('html',consolidate.ejs)
-app.set('view engine','html')
+app.engine("html", consolidate.ejs);
+app.set("view engine", "html");
 //app.set('view engine','css')
-app.set('views',__dirname + '/dist')
+app.set("views", __dirname + "/dist");
 
 //app.use('/',express.static('./'))
 //提供静态服务
-app.use('/app',express.static('./dist'))
+app.use("/app", express.static("./dist"));
 //app.use('/', proxy({target: 'http://127.0.0.1:8868/', changeOrigin: true}));
 
 // app.use('/app/index',function(req,res,next){
 //   res.render('index.html')
 // })
-app.use('*',function(req,res,next){
-  res.render('index.html')
-})
+app.use("*", function(req, res, next) {
+  res.render("index.html");
+});
 // app.use('/app/style.css',function(req,res,next){
 //   res.sendFile(__dirname + '/dist/style.css')
 // })
@@ -29,13 +29,13 @@ app.use('*',function(req,res,next){
 //   res.sendFile(__dirname + '/dist/demo.js')
 // })
 
-
-
-app.listen(port,function(err){
-  if(err){
-    throw err
-  }else{
-    //open(`http://127.0.0.1:${port}/app/index`)
-    console.log(`==> 🌎  Listening on ${port}. Open up http://localhost:${port}/ in your browser.`)
+app.listen(port, function(err) {
+  if (err) {
+    throw err;
+  } else {
+    open(`http://127.0.0.1:${port}/app/index`);
+    console.log(
+      `==> 🌎  Listening on ${port}. Open up http://localhost:${port}/ in your browser.`
+    );
   }
-})
+});
